8b045ce4b3ab8578d8b5e1e48ee0ae897b427ba7
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-10-14 17:26:44 +0300

    update
    Update test_1.py
    Update main.py
    update x2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import sys
+
+
+if sys.argv.count("-git") > 0:
+    sys.path.append(__file__.rstrip("main.py"))
+
+
 def main():
     print("hello, world!")
 
 
 def run_test() -> bool:
     try:
+        print("")
         main()
         return True
     except Exception:
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,15 @@
-from ..main import run_test
+import inspect
+import os
+import sys
+
+
+def import_tests():
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.insert(0, parent_dir)
 
 
 def test_1():
+    import_tests()
+    from main import run_test
     assert run_test() is True
