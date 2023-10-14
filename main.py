235b29ad875ba9d5b840f0cdcfa56da3a79ab4e0
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-10-14 17:18:54 +0300

    run tests

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello, world!")
+def main():
+    print("hello, world!")
+
+
+def run_test() -> bool:
+    try:
+        main()
+        return True
+    except Exception:
+        return False
+    
---
+++ tests/test_1.py
@@ -0,0 +1,5 @@
+from main import run_test
+
+
+def test_1():
+    assert run_test() is True
