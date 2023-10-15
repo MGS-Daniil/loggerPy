1ebb25236723422d5b699862e7064806831a5d37
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-10-15 11:44:12 +0300

    Update main.py

--- python_logging_tools/main.py
+++ python_logging_tools/main.py
@@ -1,14 +1,19 @@
+from logging import Logger
 import sys
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="[%(name)s]> %(levelname)s - %(message)s")
 
-__all__ = ["Logger"]
+__all__ = ["LoggingTools"]
 
-class Logger:
-    name: str | None
+
+class LoggingTools:
+    """
+    A class that provides logging functionality.
+    """
+    _name: str | None
     level: int | None
-    logger: logging.Logger | None
+    __logger: logging.Logger | None
 
     def __new__(cls, name: str, level: int, *args, **kwargs):
         if name is None:
@@ -20,26 +25,51 @@ class Logger:
         object_ = super().__new__(cls, name, level)
         return object_
 
-    def __init__(self, name: str, level: int):
-        self.name = name
-        self.level = level
-        self.logger = logging.getLogger(self.name)
-        self.logger.setLevel(self.level)
+    def __init__(self,
+                 name: str,
+                 level: int,
+                 file: str | None = None,
+                 filemode: str = "a",
+                 log_format: str | None = "{time} [name]> level - message"):
+        self._name = name
+        self.level: int = level
+        if file is not None:
+            self.to_file = file
+
+            logging.basicConfig(format=log_format, filemode=filemode, filename=file)
+        self.__logger: Logger = logging.getLogger(self._name)
+        self.__logger.setLevel(self.level)
+
+    def to_file(self, file):
+        self.__logger.addHandler(logging.FileHandler(file))
 
     def debug(self, msg, *args, **kwargs):
-        self.logger.debug(msg)
+        self.__logger.debug(msg)
 
-    def info(self, msg, *args, **kwargs):
-        self.logger.info(msg)
+    def __log(self, msg, level, *args, **kwargs):
+        self.__logger.log(msg=msg, level=level)
 
-    def warning(self, msg, *args, **kwargs):
-        self.logger.warning(msg)
+    def info(self, msg):
+        if self.to_file is True:
+            self.__logger.info(msg)
+        else:
+            self.__log(msg, logging.INFO)
 
-    def error(self, msg, *args, **kwargs):
-        self.logger.error(msg)
+    def warning(self, msg):
+        self.warning.__doc__ = 'Logs an warning message.'
+        self.__log(msg, logging.WARNING)
 
-    def critical(self, msg, *args, **kwargs):
-        self.logger.critical(msg)
+    def error(self, msg):
+        self.error.__doc__ = 'Logs an error message.'
+        self.__log(msg, logging.ERROR)
+
+    def critical(self, msg):
+        self.critical.__doc__ = 'Logs a critical message.'
+        self.__log(msg, logging.CRITICAL)
+
+    def get_logger(self) -> Logger | None:
+        """Returns the logger object"""
+        return self.__logger
 
 
 # GitHub actions and tests
